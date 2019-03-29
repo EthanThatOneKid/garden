390b01dd1656511ef6a78521aeda6786f6540150
Ethan <ethan.r.davidson@gmail.com>
2019-03-28 19:02:05 -0700

    made print method

--- main.go
+++ main.go
@@ -2,14 +2,28 @@ package main
 
 import (
   "fmt"
+  // ui "github.com/manifoldco/promptui"
   plant "./plant"
 )
 
 func main() {
 
-  flower := plant.Plant{"flower", []string{"A"}, []string{"A", "B"}, []string{"AB", "A"}}
+  flower := plant.Plant{
+    Name: "flower",
+    Gens: []string{"A"},
+    GrowthConfigX: []string{"A", "B"},
+    GrowthConfigY: []string{"AB", "A"},
+  }
   flower.Grow(5)
-  flower.Phase([]string{"A"}, []string{"F"})
+  // flower.Phase([]string{"A"}, []string{"F"}, 2)
   fmt.Println(flower.Gens)
+  fmt.Println(flower.Print())
+
+  // prompt := ui.Prompt{
+	// 	Label:    "Number",
+	// 	// Validate: validate,
+	// }
+  // res, _ := prompt.Run()
+  // fmt.Println(res)
 
 }
--- plant/plant.go
+++ plant/plant.go
@@ -3,16 +3,54 @@ package plant
 import (
   // "fmt"
   // "math"
-  // "strings"
+  "strings"
 )
+
+func getOffset(prevLen, prevOff, curLen int) (genOff int) {
+  genOff = prevOff + int((prevLen / 2) - (curLen / 2))
+  if genOff < 0 {
+    genOff = 0
+  }
+  return
+}
 
 type Plant struct {
   Name string
   Gens, GrowthConfigX, GrowthConfigY []string
 }
 
-func (p *Plant) Phase(phaseConfigX []string, phaseConfigY []string) {
-  for i := range p.Gens {
+func (p Plant) Print() (string) {
+  result := ""
+  prevGenLen, prevOff := 0, 0
+  for i := len(p.Gens) - 1; i >= 0; i-- {
+    gimmeRow := ""
+    chars := strings.Split(string(p.Gens[i]), "")
+    // TODO : for each character, see what translate to for rendering bc rn its hardcoded
+    for j := range chars {
+      gimme := string(chars[j])
+      if gimme == "A" {
+        gimmeRow += "\\"
+      }
+      if gimme == "B" {
+        gimmeRow += "/"
+      }
+      if gimme == "F" {
+        gimmeRow += "o"
+      }
+    }
+    curLen := len(p.Gens[i])
+    genOffset := getOffset(prevGenLen, prevOff, curLen)
+    prevGenLen = curLen
+    prevOff = genOffset
+    result += strings.Repeat(" ", genOffset) + gimmeRow + "\n"
+  }
+  return result
+}
+
+func (p *Plant) Phase(phaseConfigX []string, phaseConfigY []string, layers int) {
+  from := len(p.Gens) - 1
+  to := from - layers
+  for i := from; i > to; i-- {
     accumulator := ""
     for j := range p.Gens[i] {
       cur := string(p.Gens[i][j])
